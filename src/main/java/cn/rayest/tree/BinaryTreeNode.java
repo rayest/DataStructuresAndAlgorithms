e878f8c361dfcadd83fa0f862d1e7a1407bd437d
Rayest <yi@rayest.cn>
2016-08-11 17:14:36 +0800

    声明一个二叉树

---
+++ src/main/java/cn/rayest/tree/BinaryTreeNode.java
@@ -0,0 +1,37 @@
+package cn.rayest.tree;
+
+/**
+ * Created by Rayest on 2016/8/11 0011.
+ */
+public class BinaryTreeNode {
+    private int data;
+    private BinaryTreeNode left;
+    private BinaryTreeNode right;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public BinaryTreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryTreeNode left) {
+        this.left = left;
+    }
+
+    public BinaryTreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryTreeNode right) {
+        this.right = right;
+    }
+
+
+
+}
