0b856da4c66177ff67f1e88f9113263daa8c2278
Rayest <yi@rayest.cn>
2016-08-11 15:17:23 +0800

    遍历链表

--- src/main/java/cn/rayest/list/ListNode.java
+++ src/main/java/cn/rayest/list/ListNode.java
@@ -3,9 +3,14 @@ package cn.rayest.list;
 /**
  * Created by Rayest on 2016/8/11 0011.
  */
+
+// 声明一个单向链表
 public class ListNode {
     private int data;
     private ListNode next;
+
+    public ListNode() {
+    }
 
     public ListNode(int data) {
         this.data = data;
@@ -25,5 +30,16 @@ public class ListNode {
 
     public void setNext(ListNode next) {
         this.next = next;
+    }
+
+    // 遍历链表
+    public int ListLength(ListNode headNode){
+        int length = 0;
+        ListNode currentNOde = headNode;
+        while (currentNOde != null){
+            length++;
+            currentNOde = currentNOde.getNext();
+        }
+        return length;
     }
 }
